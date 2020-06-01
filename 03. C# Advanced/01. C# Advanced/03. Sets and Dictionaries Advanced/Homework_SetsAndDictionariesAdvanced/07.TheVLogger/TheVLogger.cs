6205371382e52ccad343ad71fa905d3acc22d127
Biser Hristov <bhristov83@gmail.com>
2020-06-02 00:05:21 +0300

    The V-Logger (FULL SOLUTION 100/100)

--- 03. C# Advanced/01. C# Advanced/03. Sets and Dictionaries Advanced/Homework_SetsAndDictionariesAdvanced/07.TheVLogger/TheVLogger.cs
+++ 03. C# Advanced/01. C# Advanced/03. Sets and Dictionaries Advanced/Homework_SetsAndDictionariesAdvanced/07.TheVLogger/TheVLogger.cs
@@ -4,16 +4,14 @@ using System.Linq;
 
 namespace _07.TheVLogger
 {
-    class Program
+    class TheVLogger
     {
         static void Main(string[] args)
         {
             string[] input = Console.ReadLine()
                 .Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
-            //<person <followers>
-            //Dictionary<string, List<string>> followers = new Dictionary<string, List<string>>();
-            //Dictionary<string, int> following = new Dictionary<string, int>();
+            //person <"followers"/"following", List of followers/following
             Dictionary<string, Dictionary<string, List<string>>> dict = new Dictionary<string, Dictionary<string, List<string>>>();
 
             while (input[0] != "Statistics")
@@ -24,6 +22,8 @@ namespace _07.TheVLogger
                 {
                     if (dict.ContainsKey(input[0]))
                     {
+                        input = Console.ReadLine()
+             .Split(' ', StringSplitOptions.RemoveEmptyEntries);
                         continue;
                     }
 
@@ -31,10 +31,8 @@ namespace _07.TheVLogger
                     dict[input[0]].Add("following", new List<string>());
                     dict[input[0]].Add("followers", new List<string>());
                 }
-                //<person <followers, following>>
                 else if (command == "followed")
                 {
-                    //bool areExisting = true;
                     if (!dict.ContainsKey(input[0]) || !dict.ContainsKey(input[2]))
                     {
                         input = Console.ReadLine()
@@ -42,7 +40,6 @@ namespace _07.TheVLogger
                         continue;
                     }
 
-                    // bool isNotFollowingHimself = true;
                     if (input[0] == input[2])
                     {
                         input = Console.ReadLine()
@@ -50,8 +47,7 @@ namespace _07.TheVLogger
                         continue;
                     }
 
-                    // bool isNotAlreadyFollowing = true;
-                    if (dict[input[0]]["following"].Contains(input[2]))
+                    if (dict[input[2]]["followers"].Contains(input[0]))
                     {
                         input = Console.ReadLine()
                                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
@@ -71,8 +67,20 @@ namespace _07.TheVLogger
                 .ThenBy(x => x.Value["following"].Count)
                 .ToDictionary(x => x.Key, y => y.Value);
 
-           // var maxCount = followers.Max(x => x.Value.Count);
             Console.WriteLine($"The V-Logger has a total of {dict.Keys.Count} vloggers in its logs.");
+            int counter = 1;
+            foreach (var kvp in dict)
+            {
+                Console.WriteLine($"{counter}. {kvp.Key} : {kvp.Value["followers"].Count} followers, {kvp.Value["following"].Count} following");
+                if (counter == 1)
+                {
+                    foreach (var item in kvp.Value["followers"].OrderBy(x=>x))
+                    {
+                        Console.WriteLine($"*  {item}");
+                    }
+                }
+                counter++;
+            }
         }
     }
 }
