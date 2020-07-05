b1fba1b88177a97ba8570df67129836f3a9639a8
Biser Hristov <bhristov83@gmail.com>
2020-07-05 03:04:15 +0300

    Added Task solution for "Animals"

---
+++ 03. C# Advanced/02. C# OOP/01.Inheritance/Homework/Animals/Animal.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    abstract class Animal
+    {
+        private string name;
+        private int age;
+        private string gender;
+        public Animal(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+        protected string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                this.name = value;
+            }
+        }
+
+        protected int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            private set
+            {
+
+                bool isBlank = string.IsNullOrWhiteSpace(value.ToString());
+                
+                if (value <= 0 || isBlank || string.IsNullOrEmpty(value.ToString()))
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                this.age = value;
+            }
+        }
+
+        protected string Gender
+        {
+            get
+            {
+                return this.gender;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value) ||
+                    string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                this.gender = value;
+            }
+        }
+
+        public virtual string ProduceSound()
+        {
+            return "";
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(this.GetType().Name.ToString());
+            sb.AppendLine($"{this.Name} {this.Age} {this.Gender}");
+            sb.AppendLine(this.ProduceSound());
+            return sb.ToString().Trim();
+        }
+    }
+}
--- 03. C# Advanced/02. C# OOP/01.Inheritance/Homework/Animals/StartUp.cs
+++ 03. C# Advanced/02. C# OOP/01.Inheritance/Homework/Animals/StartUp.cs
@@ -1,10 +1,104 @@
-﻿namespace Animals
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
 {
     public class StartUp
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            
+            List<Animal> animals = new List<Animal>();
+            string type = Console.ReadLine();
+            StringBuilder result = new StringBuilder();
+
+
+            while (type != "Beast!")
+            {
+                try
+                {
+                    string[] args = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                    //string name = args[0];
+                    //int age = int.Parse(args[1]);
+                    //string gender = args[2];
+
+                    string name = string.Empty;
+                    int age = 0;
+                    string gender = string.Empty;
+
+                    for (int i = 0; i < args.Length; i++)
+                    {
+
+                        bool isNumeric = int.TryParse(args[i], out int parsedAge);
+
+                        if (!isNumeric)
+                        {
+                            if (args[i] != "Male" &&
+                            args[i] != "Female")
+                            {
+
+                                name = args[i];
+                            }
+                            else
+                            {
+                                gender = args[i];
+                            }
+
+                        }
+                        else
+                        {
+                            age = parsedAge;
+                        }
+
+
+                    }
+
+
+                    switch (type)
+                    {
+                        case "Dog":
+                            var dog = new Dog(name, age, gender);
+                            result.AppendLine(dog.ToString());
+                            break;
+
+                        case "Cat":
+                            var cat = new Cat(name, age, gender);
+                            result.AppendLine(cat.ToString());
+                            break;
+
+                        case "Frog":
+                            var frog = new Frog(name, age, gender);
+                            result.AppendLine(frog.ToString());
+                            break;
+
+                        case "Kitten":
+                            var kitten = new Kitten(name, age);
+                            result.AppendLine(kitten.ToString());
+                            break;
+
+                        case "Tomcat":
+                            var tomcat = new Tomcat(name, age);
+                            result.AppendLine(tomcat.ToString());
+                            break;
+
+                        default:
+                            throw new ArgumentException("Invalid input!");
+
+                    }
+                }
+                catch (Exception m)
+                {
+                    result.AppendLine(m.Message);
+                }
+
+
+
+                type = Console.ReadLine();
+            }
+
+            Console.WriteLine(result.ToString().Trim());
+
         }
     }
 }
