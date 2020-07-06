09ea396090a51c72ae70d2c654d085567c10dc7b
Biser Hristov <bhristov83@gmail.com>
2020-07-07 00:45:47 +0300

    Add unfinished solution for 04.PizzaCalories

---
+++ 03. C# Advanced/02. C# OOP/02.Encapsulation/Homework/Homework_Encapsulation/P04.PizzaCalories/Dough.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace P04.PizzaCalories
+{
+    public class Dough
+    {
+        private const int MIN_WEIGHT = 1;
+        private const int MAX_WEIGHT = 200;
+        private const double DEFAULT_CALORIES = 2;
+        private string flourType;
+        private string bakingTechnique;
+        private int weight;
+        private Dictionary<string, double> validFlourType;
+        private Dictionary<string, double> validBakingTechnique;
+
+        public Dough(string flourType, string bakingTechnique, int weight)
+        {
+            this.validBakingTechnique = new Dictionary<string, double>();
+            this.validFlourType = new Dictionary<string, double>();
+            this.LoadBakingtechnique();
+            this.LoadFlourType();
+            this.FlourType = flourType;
+            this.BakingTechnique = bakingTechnique;
+            this.Weight = weight;
+
+
+        }
+        public int Weight
+        {
+            get => this.weight;
+            set
+            {
+                if (value < MIN_WEIGHT || value > MAX_WEIGHT)
+                {
+                    throw new ArgumentException($"Dough weight should be in the range [{MIN_WEIGHT}..{MAX_WEIGHT}].");
+                }
+
+                this.weight = value;
+            }
+        }
+        public string FlourType
+        {
+            get => this.flourType;
+            private set
+            {
+                if (!validFlourType.ContainsKey(value))
+                {
+                    throw new ArgumentException("Invalid type of dough.");
+
+                }
+
+                this.flourType = value;
+
+            }
+
+        }
+        public string BakingTechnique
+        {
+            get => this.bakingTechnique;
+            private set
+            {
+                if (!validBakingTechnique.ContainsKey(value))
+                {
+                    throw new ArgumentException("Invalid type of dough.");
+
+                }
+                this.bakingTechnique = value;
+            }
+
+
+        }
+
+        private void LoadFlourType()
+        {
+            this.validFlourType.Add("White", 1.5);
+            this.validFlourType.Add("Wholegrain", 1.0);
+
+        }
+
+        private void LoadBakingtechnique()
+        {
+            this.validBakingTechnique.Add("Crispy", 0.9);
+            this.validBakingTechnique.Add("Chewy", 1.1);
+            this.validBakingTechnique.Add("Homemade", 1.0);
+
+        }
+
+        public double CalculateCalories()
+        {
+            return DEFAULT_CALORIES * this.weight * validFlourType[this.FlourType] * validBakingTechnique[this.bakingTechnique];
+        }
+
+    }
+}
---
+++ 03. C# Advanced/02. C# OOP/02.Encapsulation/Homework/Homework_Encapsulation/P04.PizzaCalories/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace P04.PizzaCalories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dough = new Dough("White", "Chewy", 250);
+            Console.WriteLine(dough.CalculateCalories());
+        }
+    }
+}
