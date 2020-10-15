92779eecbccf52f2a1fce0993773ee53da9e8e44
Biser Hristov <bhristov83@gmail.com>
2020-10-16 02:03:17 +0300

    Solutions for task 01 and 02 (100/100)

---
+++ 04. JS Advanced/01. JS Advanced/09.Prototypes and Inheritance/Homework_Prototypes And Inheritance/01.ArrayExtension.js
@@ -0,0 +1,41 @@
+(function solve() {
+
+    Array.prototype.last = function () {
+        return this[this.length - 1]
+    }
+
+    Array.prototype.skip = function (n) {
+        let result = []
+
+        for (let index = n; index < this.length; index++) {
+            result.push(this[index]);
+
+        }
+        return result
+    }
+
+    Array.prototype.take = function (n) {
+        let result = []
+
+        for (let index = 0; index < n; index++) {
+            result.push(this[index]);
+
+        }
+        return result
+    }
+
+    Array.prototype.sum = function () {
+
+        return this.reduce((acc, x) => {
+            return acc += x;
+        }, 0)
+    }
+
+    Array.prototype.average = function () {
+
+        return this.sum() / this.length
+    }
+
+
+
+})()
