a66cbbd71170d06b674fd94c0761f22409deb4eb
Biser Hristov <bhristov83@gmail.com>
2020-10-31 22:58:10 +0200

    Solution for task 03 and 04

--- 04. JS Advanced/02. JS Applications/02.REST Services and AJAX/Homework_REST and AJAX/03.PHONEBOOK/app.js
+++ 04. JS Advanced/02. JS Applications/02.REST Services and AJAX/Homework_REST and AJAX/03.PHONEBOOK/app.js
@@ -1,5 +1,51 @@
 function attachEvents() {
-    console.log('TODO...');
+
+    let ul = document.querySelector('#phonebook');
+    let loadBtn = document.querySelector('#btnLoad');
+    loadBtn.addEventListener('click', (e) => {
+        ul.innerHTML = ''
+        fetch(getURL())
+            .then(response => response.json())
+            .then(data => {
+                Object.entries(data).forEach(([key, value]) => {
+                    let li = document.createElement('li');
+                    let button = document.createElement('button');
+                    button.textContent = 'Delete';
+                    li.textContent = `${value.person}:${value.phone}`
+                    li.appendChild(button)
+                    ul.appendChild(li);
+
+                    button.addEventListener('click', (buttonEvent) => {
+                        fetch(getURL(key), { method: 'DELETE' })
+                        buttonEvent.target.parentElement.remove();
+                    })
+
+                });
+            })
+    })
+
+    let createBtn = document.querySelector('#btnCreate');
+    createBtn.addEventListener('click', () => {
+        let name = document.querySelector('#person');
+        let phone = document.querySelector('#phone');
+
+        let person = {
+            person: name.value,
+            phone: phone.value
+        }
+
+        fetch(getURL(), { method: 'POST', body: JSON.stringify(person) })
+
+        name.value = ''
+        phone.value = ''
+
+    })
+
+
+    function getURL(postfix) {
+        let result = `https://phonebook-nakov.firebaseio.com/phonebook` + (postfix ? `/${postfix}` : '') + `.json`
+        return result;
+    }
 }
 
 attachEvents();
--- 04. JS Advanced/02. JS Applications/02.REST Services and AJAX/Homework_REST and AJAX/04.MESSENGER/app.js
+++ 04. JS Advanced/02. JS Applications/02.REST Services and AJAX/Homework_REST and AJAX/04.MESSENGER/app.js
@@ -1,5 +1,32 @@
 function attachEvents() {
-    console.log('TODO...');
+    let sendBtn = document.querySelector('#submit');
+    let refreshBtn = document.querySelector('#refresh');
+    let authorEl = document.querySelector('#author');
+    let contentEl = document.querySelector('#content');
+    let textArea = document.querySelector('#messages');
+
+    let url = 'https://rest-messanger.firebaseio.com/messanger.json'
+
+    sendBtn.addEventListener('click', () => {
+        let message = {
+            author: authorEl.value,
+            content: contentEl.value
+        }
+
+        fetch(url, { method: 'POST', body: JSON.stringify(message) })
+        authorEl.value = ''
+        contentEl.value = ''
+    })
+
+    refreshBtn.addEventListener('click', () => {
+        textArea.value = '';
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                Object.values(data).forEach(m => textArea.value += `${m.author}: ${m.content}\n`)
+            })
+    })
+
 }
 
 attachEvents();
