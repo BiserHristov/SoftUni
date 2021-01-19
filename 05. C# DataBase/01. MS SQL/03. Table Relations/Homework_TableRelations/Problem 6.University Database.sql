870fad6d88868c6df6c962913fe1ac83ae7ed8fe
Biser Hristov <bhristov83@gmail.com>
2021-01-20 01:03:19 +0200

    Homework solutions for all tasks (1-9)

---
+++ 05. C# DataBase/01. MS SQL/03. Table Relations/Homework_TableRelations/Problem 6.University Database.sql
@@ -0,0 +1,35 @@
+CREATE TABLE Majors
+(
+MajorID INT PRIMARY KEY IDENTITY,
+[Name] VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Students
+(
+StudentID INT PRIMARY KEY IDENTITY,
+StudentNumber CHAR(10),
+StudentName VARCHAR(50),
+MajorID INT FOREIGN KEY REFERENCES Majors(MajorID)
+)
+
+CREATE TABLE Payments
+(
+PaymentID INT PRIMARY KEY IDENTITY,
+PaymentDate DATE,
+PaymentAmount DECIMAL(15,2),
+StudentID INT FOREIGN KEY REFERENCES Students(StudentID)
+)
+
+CREATE TABLE Subjects
+(
+SubjectID INT PRIMARY KEY IDENTITY,
+SubjectName VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Agenda
+(
+StudentID INT FOREIGN KEY REFERENCES Students(StudentID),
+SubjectID INT FOREIGN KEY REFERENCES Subjects(SubjectID),
+
+CONSTRAINT PK_StudentID_SubjectID PRIMARY KEY (StudentID,SubjectID)
+)
