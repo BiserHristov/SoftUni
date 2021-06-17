f83d697cf6cf8e748ac98886653fdc6cd1ffd4c8
Biser Hristov <bhristov83@gmail.com>
2021-06-17 15:23:30 +0300

    Automatically implement POST request with reflection

---
+++ 06. C# Web/01. C# Web Basics/02.Web Server - Asynchronous Processing/MyWebServer/MyWebServer/Controllers/HttpMethodAttribute.cs
@@ -0,0 +1,17 @@
+﻿using MyWebServer.HTTP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWebServer.Controllers
+{
+    public abstract class HttpMethodAttribute : Attribute
+    {
+        public HttpMethodAttribute(HttpMethod httpMethod)
+       => this.HttpMethod = httpMethod;
+
+        public HttpMethod HttpMethod { get; }
+    }
+}
